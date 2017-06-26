0204355bbd13a23a7bcc0b054d837b993e67e101
kaibinyuan <kaibinyuan@126.com>
2017-06-26 21:57:43 +0800

    db class add

---
+++ 2016-09-10/web_portal/userDB.py
@@ -0,0 +1,85 @@
+#!/usr/bin/env python
+# -*- coding: utf-8 -*-
+# @Date    : 2016-08-13 18:59:48
+# @Author  : yang ke (jack_keyang@163.com)
+
+import MySQLdb as mysql
+
+
+class UserDB:
+    def __init__(self):
+        self.host = 'localhost'
+        self.port = '3306'
+        self.user = 'root'
+        self.password = '123456'
+        self.db = 'reboot'
+
+        self.conn = mysql.connect(host=self.host,
+                                  user=self.user,
+                                  passwd=self.password,
+                                  db=self.db, charset="utf8")
+        self.conn.autocommit(True)
+        self.curs = self.conn.cursor()
+
+    def userlist(self, fields):
+        curs = self.curs
+        sql = "select %s from users" % ",".join(fields)
+        curs.execute(sql)
+        result = curs.fetchall()
+        u_list = [dict((k, row[i]) for i, k in enumerate(fields)) for row in result]
+        return u_list
+
+    def getone(self, where):
+        curs = self.curs
+        fields = ["id", "name", "name_cn", "mobile", "email", "role", "status"]
+        condition = '%s = "%s"' % (where.keys()[0], where.values()[0])
+        sql = "select %s from users where %s" % (",".join(fields), condition)
+        curs.execute(sql)
+        result = curs.fetchone()
+        u_dict = dict((k, result[i])for i, k in enumerate(fields))
+        return u_dict
+
+    def modfiy(self, fields):
+        curs = self.curs
+        conn = self.conn
+        data = ",".join(["%s='%s'" % (k, v) for k, v in fields.items()])
+        sql = "update users set %s where id=%s " % (data, fields["id"])
+        curs.execute(sql)
+        conn.commit()
+
+    def adduser(self, fields):
+        curs = self.curs
+        conn = self.conn
+        sql = "insert into users(%s)values('%s')" % (",".join(fields.keys()), "','".join(fields.values()))
+        curs.execute(sql)
+        conn.commit()
+
+    def delete(self, uid):
+        curs = self.curs
+        conn = self.conn
+        sql = "delete from users where id=%s" % uid
+        curs.execute(sql)
+        conn.commit()
+
+    def checkuser(self, dict,fields):
+        curs = self.curs
+        sql = "select %s from users where %s='%s'" % (','.join(fields), dict.keys()[0], dict.values()[0])
+        curs.execute(sql)
+        result = curs.fetchone()
+        #res = dict((k, result[i]) for i, k in enumerate(fields))
+        if result:
+            res = {}
+            for i, k in enumerate(fields):
+                res[k] = result[i]
+            return res
+        else:
+            res = ""
+            return res
+
+    def modpasswd(self, dict):
+        sql = "update users set password='%(password)s' where id=%(id)s" % dict
+        curs = self.curs
+        conn = self.conn
+        curs.execute(sql)
+        conn.commit()
+
